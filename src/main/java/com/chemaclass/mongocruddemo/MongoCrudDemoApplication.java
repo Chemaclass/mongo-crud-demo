64632301e4c4745aaccd5e8105b3c21c702b8c25
Chemaclass <chemaclass@outlook.es>
2020-01-18 22:14:33 +0100

    Spring boot init commit

---
+++ src/main/java/com/chemaclass/mongocruddemo/MongoCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chemaclass.mongocruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoCrudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chemaclass/mongocruddemo/MongoCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chemaclass.mongocruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
