7e2281f82ed5ad349923afc446d3187eaacd497b
Chemaclass <chemaclass@outlook.es>
2020-01-20 11:49:18 +0100

    Added functional test for ItemController::create endpoint

---
+++ src/test/java/com/chemaclass/mongocruddemo/controller/ItemControllerTest.java
@@ -0,0 +1,35 @@
+package com.chemaclass.mongocruddemo.controller;
+
+import com.chemaclass.mongocruddemo.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ItemRepository repository;
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(post("/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\": \"p5\",\"price\": \"666\"}"));
+
+        assertEquals(1, repository.count());
+    }
+}
